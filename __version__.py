0bf4170b075a0c0b37bad344861332958db6ab54
Fibogacci <mxlinuxpl@gmail.com>
2025-06-22 13:45:49 +0200

    Add missing __version__.py file

---
+++ __version__.py
@@ -0,0 +1,16 @@
+"""Version information for llama.cpp automatic installer"""
+
+__version__ = "1.0.0"
+__version_info__ = (1, 0, 0)
+
+# Version information
+VERSION_MAJOR = 1
+VERSION_MINOR = 0
+VERSION_PATCH = 0
+VERSION_STRING = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
+
+# Project metadata
+PROJECT_NAME = "Automatic llama.cpp installer"
+PROJECT_AUTHOR = "Fibogacci"
+PROJECT_URL = "https://fibogacci.pl"
+PROJECT_LICENSE = "MIT"
